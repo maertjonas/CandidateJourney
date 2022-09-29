858428b458129898fcde2dc5d6c0b92d6f17594f
Jonas Maertens <Jonas.Maertens@ordina.be>
2022-09-29 09:11:22 +0200

    Add project files.

---
+++ Domain/Interest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Interest
+    {
+        public string Name { get; set; }
+
+        public Interest()
+        {
+                
+        }
+    }
+}
